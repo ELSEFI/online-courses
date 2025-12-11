598ac31c190b5d9b271ce3548cf49e74220b2eae
ELSEFI <mohamedelsafy.as@gmail.com>
2025-12-11 23:24:11 +0200

    Create Controller Allow Admin To Delete User

--- Backend/src/controllers/adminController.js
+++ Backend/src/controllers/adminController.js
@@ -270,3 +270,18 @@ exports.addUser = async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 };
+
+exports.deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.userId);
+  if (!user) return res.status(400).json({ message: "User Not Founded" });
+  const instructor = await instructorProfile.findOne({ userId: user._id });
+  if (!instructor) {
+    await User.findByIdAndDelete(user._id);
+    return res.status(200).json({ message: "User Deleted Successfully" });
+  }
+
+  await instructorProfile.findByIdAndDelete(instructor._id);
+  await User.findByIdAndDelete(user._id);
+
+  res.status(200).json({ message: "User Deleted Successfully" });
+};
